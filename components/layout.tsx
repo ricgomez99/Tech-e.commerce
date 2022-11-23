d2d72691f953c063c5908888ec35d6af65bee986
Martina Girollet <martinagirolletgmail.com>
2022-11-23 17:28:28 -0300

    layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ title, children }: Props) {
     return(
       <div>
         <Head>
-          <title>E-commerce {title? `|${title}` : ""}</title>
+          <title>E-commerce{title? `| ${title}` : ""}</title>
         </Head>
         <NavBar/>
         <div>{children}</div>

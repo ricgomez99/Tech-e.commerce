baf4bd11b3687e62fc3a501edb7c99e8da01c029
NicoxRC <nicorojas005@gmail.com>
2022-11-23 18:15:56 -0500

    trycatch postProduct

--- services/otherEndPoint.ts
+++ services/otherEndPoint.ts
@@ -1,21 +1,25 @@
-
 export async function postProduct(data: BodyInit) {
-  const request = await fetch(
-    "https://techproductsshop-production.up.railway.app/products",
-    {
-      method: "POST",
-      body: data,
-    }
-  );
-  return request.json();
+  try {
+    const request = await fetch(
+      "https://techproductsshop-production.up.railway.app/products",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    return request.json();
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function idProduct(id: string) {
-    try {
-        const json = fetch(`https://techproductsshop-production.up.railway.app/${id}`)
-        return (await json).json();
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const json = fetch(
+      `https://techproductsshop-production.up.railway.app/${id}`
+    );
+    return (await json).json();
+  } catch (error) {
+    return error;
+  }
 }
-

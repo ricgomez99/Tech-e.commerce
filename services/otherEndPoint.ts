eca46dc64ce786219efe5cd764f63c5655d404a7
NicoxRC <nicorojas005@gmail.com>
2022-11-23 18:08:49 -0500

    endpoint post product

--- services/otherEndPoint.ts
+++ services/otherEndPoint.ts
@@ -1,3 +1,10 @@
-export async function name() {
-    
+export async function postProduct(data: BodyInit) {
+  const request = await fetch(
+    "https://techproductsshop-production.up.railway.app/products",
+    {
+      method: "POST",
+      body: data,
+    }
+  );
+  return request.json();
 }

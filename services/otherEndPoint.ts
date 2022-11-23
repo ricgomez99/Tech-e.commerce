9161423d0bd75e2526af7d74c8bf3032038c4325
juanjobogado <juanjobogado13@gmail.com>
2022-11-23 20:26:03 -0300

    endpoint nameProduct

--- services/otherEndPoint.ts
+++ services/otherEndPoint.ts
@@ -1,4 +1,3 @@
-
 export async function postProduct(data: BodyInit) {
   const request = await fetch(
     "https://techproductsshop-production.up.railway.app/products",
@@ -11,11 +10,21 @@ export async function postProduct(data: BodyInit) {
 }
 
 export async function idProduct(id: string) {
-    try {
-        const json = fetch(`https://techproductsshop-production.up.railway.app/${id}`)
-        return (await json).json();
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const json = fetch(
+      `https://techproductsshop-production.up.railway.app/${id}`
+    );
+    return (await json).json();
+  } catch (error) {
+    return error;
+  }
 }
 
+export async function nameProduct(name: string) {
+  try {
+    const json = await fetch(`https://techproductsshop-production.up.railway.app?title=${name}`)
+    return json.json();
+  } catch (error) {
+    return error;
+  }
+}

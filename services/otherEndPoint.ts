92244eac5737828e2605b65e119af3728275bc53
Jotanjo <maza011000@gmail.com>
2022-11-23 20:03:23 -0300

    adding the categories fetch

--- services/otherEndPoint.ts
+++ services/otherEndPoint.ts
@@ -1,3 +1,9 @@
 export async function name() {
     
 }
+
+export async function getCategories() {
+    const request = fetch("https://techproductsshop-production.up.railway.app/categories")
+                    .then(response => response.json())
+                    .then(data => data.categories)
+}

60a3a8e214539f2e49daa50ce97e4c521a305eae
Andres <ricardo.gomez.restrepo@gmail.com>
2022-11-23 16:12:43 -0500

    index-store-productTitle-render

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,7 +1,29 @@
-export default function IndexStore() {
-    return(
-        <div>
-            <h1>Here all the products, Home Page</h1>
-        </div>
-    );
+import { getProducts } from "../../services/productService";
+
+type Data = {
+  products: any[];
 };
+
+export default function IndexStore({ products }: Data) {
+  return (
+    <div>
+      <h1>Here all the products, Home Page</h1>
+      <div>
+        {products &&
+          products.map((product: any) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await getProducts();
+
+  return {
+    props: {
+      products: res,
+    },
+  };
+}

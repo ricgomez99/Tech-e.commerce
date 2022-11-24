56018435a53b7d8f94af1150e772778c2df4bdda
NicoxRC <nicorojas005@gmail.com>
2022-11-24 17:11:16 -0500

    creating singin

--- components/singinmodal.tsx
+++ components/singinmodal.tsx
@@ -1,9 +1,31 @@
-function singInModal() {
+import {getSession} from 'next-auth/react'
+
+function singInModal({session}:any) {
   return (
     <div>
-      botones
+      bottons
     </div>
   )
+}
+
+
+export const getServerSideProps = async (context:any) => {
+  const session =  await getSession(context)
+
+
+  //esta opcion lo redireccion si no tiene credenciales
+  // if (!session) return {
+  //   redirect:{
+  //     detination: '/login',
+  //     permanent: false
+  //   }
+  // }
+
+  return {
+    props:{
+      session
+    }
+  }
 }
 
 export default singInModal

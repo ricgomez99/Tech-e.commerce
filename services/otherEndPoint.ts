3c44da372739e7188bcd76a9f7df4e8cb1e5693b
userJAMT <jmendez4tob@gmail.com>
2022-11-23 21:12:43 -0300

    updateProduct and getProducst with filters

--- services/otherEndPoint.ts
+++ services/otherEndPoint.ts
@@ -1,3 +1,30 @@
 export async function name() {
     
 }
+
+
+//Esto hace lo mismo que getProducts, pero tiene anexado los filtros
+export async function getProducts2(data: any) {
+  try {
+    let query: string = ''
+    if (Object.keys(data).length > 0) {
+      let array: Array<string> = []
+      for (let key in data) {
+        array.push([key, data[key]].join("="))
+      }
+      query = array.join("&")
+    }
+    if (!query.length) {
+      const request = await fetch("https://techproductsshop-production.up.railway.app/products");
+      const products = await request.json();
+      return products;
+    }
+    else {
+      const request = await fetch(`https://techproductsshop-production.up.railway.app/products?${query}`);
+      const products = await request.json();
+      return products;
+    }
+  } catch (error) {
+    return error;
+  }
+}

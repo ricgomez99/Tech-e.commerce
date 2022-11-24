f22fd2c98f59e02f2f215c83327ddc39673eea0c
felihq <felihq24q@gmail.com>
2022-11-24 17:29:47 -0300

    Finished footer and styles (at least for now)

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,15 +7,16 @@ import styles from '../styles/footer.module.css'
 export default function Footer(){
   return (
     <div>
-      <footer className={styles.footer}>
-        <p>Copyright 2022</p>
-
-        <Link href='/about' >About</Link> 
-
-        <a href='https://github.com/ricgomez99/Tech-e.commerce'> 
-        <Image src={githubLogo} alt="img" width={48} height={48}></Image>
+      <footer>
+        <div className={styles.footer}>
+        <nav className={styles.nav}>
+        <Link className={styles.aboutus} href='/about' >About Us</Link> 
+        <p className={styles.paragraph}>Designed and built with all the love in the world <br/> by the Fullstack Street Guys team.</p>
+        <a className={styles.logo} href='https://github.com/ricgomez99/Tech-e.commerce'> 
+        <Image src={githubLogo} alt="img" width={40} height={40}></Image>
         </a>
-
+        </nav>
+        </div>
       </footer>
     </div>
   )

3f694b69f353cb9a0ab74141187d25fd5ea6b903
Antonio Michel <antoniomichel@pedros-mbp.lan>
2022-11-24 12:53:25 -0600

    Erased some useless code

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,42 +1,6 @@
-/*
-import Link from "next/link";
-import Image from "next/image";
-import Script from "next/script";
-
-//import style from "../styles/navBar.module.css";
-
-export default function NavBar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top main-navigation">
-            <Link href="/" className="ps-3 navbar-brand">
-                <Image src="/img/e-commerce.png" alt="logo" width={35} height={35} />
-            </Link>
-
-            <button className="navbar-toggler" type="button">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="container-fluid navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
-
-                <Link className="nav-link" href="/">
-                    Home
-                </Link>
-                <Link className="nav-link" href="/store">
-                    Store
-                </Link>
-                <Link className="nav-link" href="/about">
-                    About
-                </Link>
-                {/* falta agregar boton de carrito */ /*}
-</div>
-</nav>
-);
-} 
-*/
-
 import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
-
 import NavItem from "./navitem";
 
 import styles from "../styles/navbar.module.css"
@@ -53,10 +17,9 @@ const Navbar = () => {
     return (
         <header>
             <nav className={styles.navbar}>
-                <Link href={"/"}>
-                    <h1>Logo</h1>
-
-                </Link>
+            <Link href="/" className="ps-3 navbar-brand">
+                <Image src="/img/e-commerce.png" alt="logo" width={35} height={35} />
+            </Link>
                 <div
                     onClick={() => setNavActive(!navActive)}
                     className={styles.nav__menu_bar}

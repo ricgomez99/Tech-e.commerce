f46d6a016e83172bfa23a31641ef3ba917aa955c
Martina Girollet <martinagirolletgmail.com>
2022-11-25 10:16:42 -0300

    landing styles in process

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
-import Head from "next/head";
-//import Image from "next/image";
+// import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/layout";
 import Footer from "../components/footer";
 import Image from "next/image";
 import landing from '../public/Img/landing.png';
-import Carousel from "../components/carousel";
+import CarouselLanding from "../components/carousellanding";
+import { useRouter } from "next/router";
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
     <Layout>
         <div className="container-fluid text-center">
@@ -18,9 +21,8 @@ export default function Home() {
               <p className="text-start">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi rerum, quo, unde, error necessitatibus debitis tenetur tempore assumenda nostrum rem sapiente! Natus, est sint inventore exercitationem vel velit molestias. Tempora.</p>
             </div>
             <Image src={landing} alt="img" width={350}></Image>
-          <button className={styles.card}>Get Started</button>
-          <Carousel />
-          <button className={styles.card}>Get Offer</button>
+          <button className={styles.card} onClick={() => router.push("/store")}>Get Started</button>
+          <CarouselLanding/>
         </div>
         <Footer />
     </Layout>

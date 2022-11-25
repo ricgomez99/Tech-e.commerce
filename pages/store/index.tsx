4e8ccee6bf05323a0df2471470caf88364057365
juanjobogado <juanjobogado13@gmail.com>
2022-11-25 18:59:14 -0300

    comentarios en searchbar eliminados e identación

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -10,8 +10,6 @@ import stylePaginator from "../../styles/paginator.module.css";
 import SearchBar from "../../components/searchbar";
 import { nameProduct } from "../../services/productEndPoints";
 
-
-
 type Data = {
   products: any[];
 };
@@ -21,33 +19,31 @@ export default function Index({ products }: Data) {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const pageSize = 8;
 
-
   const paginateItems: any = paginate(items, currentPage, pageSize);
 
-  let response : any; 
+  let response: any;
 
-  const handleSearch = async (title: any) =>{
-  response = await nameProduct(title)
-  setItems(response);
+  const handleSearch = async (title: any) => {
+    response = await nameProduct(title);
+    setItems(response);
+  };
 
-  }
-  
   const handlePageChange = (page: any): any => {
     setCurrentPage(page);
   };
 
   useEffect(() => {
-    if(response?.length > 0){
-      setItems(response)
+    if (response?.length > 0) {
+      setItems(response);
     } else {
-      setItems(products)
+      setItems(products);
     }
-  },[response]);
+  }, [response]);
 
   return (
     <Layout>
       <h1>Store page</h1>
-      <SearchBar onSearch={handleSearch}/>
+      <SearchBar onSearch={handleSearch} />
       <div className={styledProducts.items}>
         {paginateItems &&
           paginateItems.map((product: any) => (
@@ -83,4 +79,3 @@ export async function getStaticProps() {
     },
   };
 }
-

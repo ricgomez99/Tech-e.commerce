16e8b1ff2b3cd4babe5951fce6f7016d5c05f155
juanjobogado <juanjobogado13@gmail.com>
2022-11-25 18:57:21 -0300

    comentarios en el componente searchbar eliminado

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,51 +1,29 @@
 import React, { useState } from "react";
-import { nameProduct } from "../services/productEndPoints";
-
 export interface SearchBarProps {
-    onSearch(title: string): void;
+  onSearch(title: string): void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-
-const [title, setTitle] = useState("");
-
-
-
-//   const handleSubmit = (e : any) => {
-//     e.preventDefault();
-//     if(title){
-//         nameProduct(title);
-//         console.log(nameProduct(title))
-//     } else {
-//         alert("enter correct value")
-//     }
-
-//   }
-
-//   const handleClick = () => {
-    
-//     onSearch(title);
-
-//   };
+  const [title, setTitle] = useState("");
 
   return (
-    <form 
-    onSubmit={(e) => {
+    <form
+      onSubmit={(e) => {
         e.preventDefault();
         onSearch(title);
-    }}>
-    <div>
-      <input
-        type="text"
-        value={title}
-        placeholder="Search product..."
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      {/* <button onClick={handleClick}>Search</button> */}
-      <button type="submit">Search</button>
-    </div>
+      }}
+    >
+      <div>
+        <input
+          type="text"
+          value={title}
+          placeholder="Search product..."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </div>
     </form>
   );
-}
+};
 
 export default SearchBar;

f6ea2fe870997f679d20a6be0bdb7cc85f0d27f2
Martina Girollet <martinagirolletgmail.com>
2022-11-25 12:17:06 -0300

    trying to pull

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-// import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/layout";
 import Footer from "../components/footer";
@@ -21,8 +20,10 @@ export default function Home() {
               <p className="text-start">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi rerum, quo, unde, error necessitatibus debitis tenetur tempore assumenda nostrum rem sapiente! Natus, est sint inventore exercitationem vel velit molestias. Tempora.</p>
             </div>
             <Image src={landing} alt="img" width={350}></Image>
-          <button className={styles.card} onClick={() => router.push("/store")}>Get Started</button>
-          <CarouselLanding/>
+            <div>
+              <button className={styles.card} onClick={() => router.push("/store")}>Get Started</button>
+          </div>
+            <CarouselLanding/>
         </div>
         <Footer />
     </Layout>

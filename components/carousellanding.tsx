5e7a54e277d0541ad7ce99bf47abe9ba26f770e6
Andres <ricardo.gomez.restrepo@gmail.com>
2022-11-25 10:08:59 -0500

    carousel-container-fixed

--- components/carousellanding.tsx
+++ components/carousellanding.tsx
@@ -1,4 +1,4 @@
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 import mouse from "../public/Img/mouse.png";
 import laptop from "../public/Img/laptop.png";
 import desktop from "../public/Img/desktop.png";
@@ -7,84 +7,45 @@ import display from "../public/Img/display.png";
 import headphone from "../public/Img/headphones.png";
 import keyboard from "../public/Img/keyboard.png";
 import webcam from "../public/Img/webcam.png";
-import Image from 'next/image';
+import Image from "next/image";
+import st from "../styles/Home.module.css";
 
 export default function CarouselLanding() {
   return (
-    <Carousel fade>
+    <Carousel fade className={st.car}>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={headphone}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Image className="d-block w-100" src={headphone} alt="Third slide" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={keyboard}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Image className="d-block w-100" src={keyboard} alt="Third slide" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={chair}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Image className="d-block w-100" src={chair} alt="Third slide" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={display}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Image className="d-block w-100" src={display} alt="Third slide" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={webcam}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Image className="d-block w-100" src={webcam} alt="Third slide" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={mouse}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Image className="d-block w-100" src={mouse} alt="First slide" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={laptop}
-          alt="Second slide"
-        />
+        <Image className="d-block w-100" src={laptop} alt="Second slide" />
 
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src={desktop}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
+        <Image className="d-block w-100" src={desktop} alt="Third slide" />
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
-};
+}

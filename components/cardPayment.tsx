96e92bde79bc66e09a4cf1e2b04c5367e3b2107e
Martina Girollet <martinagirolletgmail.com>
2022-11-29 19:09:54 -0300

    Payment almost done

--- components/cardPayment.tsx
+++ components/cardPayment.tsx
@@ -1,8 +1,38 @@
+import styles from "../styles/cardPayment.module.css";
+import { useAppContext } from "./statewrapper";
+import Image from "next/image";
+import { FaTrash } from "react-icons/fa";
+
 
 export default function cardPayment() {
+
+    const products = useAppContext();
+
+    
+    const handleDelete = (product: any) => {
+      products.deleteItem(product);
+    }
+
     return(
-        <div>
-            
-        </div>
+      <div>
+        {products.items?.map((p) => (
+          <div className={styles.container}>
+            <div>
+              <Image
+                src={p.image}
+                alt={p.title}
+                width={200}
+                height={150}
+              />
+            </div>
+            <div className={styles.titleContainer}>
+              <h5 className={styles.title}> {p.title}</h5>
+              <h3 className={styles.price}>US${p.price}</h3>
+              <button className="btn btn-danger" onClick={() =>handleDelete(p.id)}><FaTrash/></button>
+            </div>
+          </div>
+        ))}
+        
+      </div>
     );
 };
--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,28 +1,37 @@
 import Layout from "components/layout";
 import Footer from "components/footer";
-// import CardPayment from "components/cardPayment";
+import CardPayment from "components/cardPayment";
 import Router from "next/router";
-import React from "react"; //useContext
+import React from "react";
 import styles from "../styles/payment.module.css";
+import { useAppContext } from "components/statewrapper";
+import { MdOutlineArrowBack } from "react-icons/md";
 
 export default function Payment() {
 
+    const products = useAppContext().items;
 
-
+    const getTotal = () => {
+        const total = products.reduce((acc, item)=> acc+ item.qty * item.price, 0);
+        return total;
+    }
 
     return(
         <Layout>
             <div className={styles.containerPayment}>
-                <button onClick={() => Router.back()}>Back</button>
+                <button onClick={() => Router.back()} className="btn btn-outline-secondary"> <MdOutlineArrowBack/></button>
+                <div className={styles.title}>
+                    <h2>Payment Details</h2>
+                </div>
                 <div className={styles.containerInfo}>
-                    <div>
-                        Productos
-                        {/* <CardPayment /> */}
+                    <div className={styles.card}>
+                        <CardPayment />
                     </div>
 
-                    <div>
-                        <h5>Total price: US${}</h5>
-                        <button>Pay</button>
+                    <div className={styles.total}>
+                        <h3 className={styles.totalPrice}>Total price: US${getTotal()}</h3>
+                        <button style={{ cursor: "pointer", width: "250px", height:"50px"}}
+                            className="btn btn-success">Pay</button>
                     </div>
                 </div>
             </div>

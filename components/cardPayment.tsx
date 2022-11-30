93df3afeedf6b689ad856d91983186b6fce2b42a
Martina Girollet <martinagirolletgmail.com>
2022-11-30 13:53:53 -0300

    CardPayment component fixed

--- components/cardPayment.tsx
+++ components/cardPayment.tsx
@@ -3,36 +3,33 @@ import { useAppContext } from "./statewrapper";
 import Image from "next/image";
 import { FaTrash } from "react-icons/fa";
 
+export default function CardPayment() {
 
-export default function cardPayment() {
+  const products = useAppContext();
 
-    const products = useAppContext();
+  const handleDelete = (product: any) => {
+    products.deleteItem(product);
+  }
 
-    
-    const handleDelete = (product: any) => {
-      products.deleteItem(product);
-    }
-
-    return(
-      <div>
-        {products.items?.map((p) => (
-          <div className={styles.container}>
-            <div>
-              <Image
-                src={p.image}
-                alt={p.title}
-                width={200}
-                height={150}
-              />
-            </div>
-            <div className={styles.titleContainer}>
-              <h5 className={styles.title}> {p.title}</h5>
-              <h3 className={styles.price}>US${p.price}</h3>
-              <button className="btn btn-danger" onClick={() =>handleDelete(p.id)}><FaTrash/></button>
-            </div>
+  return(
+    <div>
+      {products.items?.map((p) => (
+        <div className={styles.container}>
+          <div>
+            <Image
+              src={p.image}
+              alt={p.title}
+              width={200}
+              height={150}
+            />
           </div>
-        ))}
-        
-      </div>
-    );
+          <div className={styles.titleContainer}>
+            <h5 className={styles.title}> {p.title}</h5>
+            <h3 className={styles.price}>US${p.price}</h3>
+            <button className="btn btn-danger" onClick={() =>handleDelete(p.id)}><FaTrash/></button>
+          </div>
+        </div>
+      ))}  
+    </div>
+  );
 };
--- pages/payment.tsx
+++ pages/payment.tsx
@@ -9,33 +9,35 @@ import { MdOutlineArrowBack } from "react-icons/md";
 
 export default function Payment() {
 
-    const products = useAppContext().items;
+  const products = useAppContext().items;
 
-    const getTotal = () => {
-        const total = products.reduce((acc, item)=> acc+ item.qty * item.price, 0);
-        return total;
-    }
+  const getTotal = () => {
+    const total = products.reduce((acc, item)=> acc+ item.qty * item.price, 0);
+    return total;
+  }
 
-    return(
-        <Layout>
-            <div className={styles.containerPayment}>
-                <button onClick={() => Router.back()} className="btn btn-outline-secondary"> <MdOutlineArrowBack/></button>
-                <div className={styles.title}>
-                    <h2>Payment Details</h2>
-                </div>
-                <div className={styles.containerInfo}>
-                    <div className={styles.card}>
-                        <CardPayment />
-                    </div>
-
-                    <div className={styles.total}>
-                        <h3 className={styles.totalPrice}>Total price: US${getTotal()}</h3>
-                        <button style={{ cursor: "pointer", width: "250px", height:"50px"}}
-                            className="btn btn-success">Pay</button>
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </Layout>
-    );
+  return(
+    <Layout>
+      <div className={styles.containerPayment}>
+        <button onClick={() => Router.back()} className="btn btn-outline-secondary">
+          <MdOutlineArrowBack/>
+        </button>
+        <div className={styles.title}>
+          <h2>Payment Details</h2>
+        </div>
+        <div className={styles.containerInfo}>
+          <div className={styles.card}>
+            <CardPayment />
+          </div>
+          <div className={styles.total}>
+            <h3 className={styles.totalPrice}>Total price: US${getTotal()}</h3>
+            <button style={{ cursor: "pointer", width: "250px", height:"50px"}} className="btn btn-success">
+              Pay
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </Layout>
+  );
 };

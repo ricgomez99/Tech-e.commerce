5694be4196ef6873bbeb9259faebb9c68fa1d61f
userJAMT <jmendez4tob@gmail.com>
2022-12-01 12:37:58 -0300

    Stripe implemented and connected with cart

--- pages/api/checkout_sessions/index.ts
+++ pages/api/checkout_sessions/index.ts
@@ -7,49 +7,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { quantity } = req.body
+    const line_items = req.body
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'Papita.jpg',
-              images:['https://media.tenor.com/PZTvw4FUaTcAAAAM/le%C5%9F.gif'],
-            },
-            unit_amount: 2000,
-          },
-          quantity: 1,
-        },
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'Banana',
-              images:['https://media.tenor.com/4_E21LSI0ogAAAAj/banana-cheerer.gif'],
-            },
-            unit_amount: 500,
-          },
-          quantity: 2,
-        },
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              
-              name: 'Mouse',
-              images: ["https://http2.mlstatic.com/D_NQ_NP_2X_979137-MLA44255818618_122020-F.webp"],
-            },
-            unit_amount: 1500,
-          },
-          quantity: 1,
-        },
-        {
-          price: process.env.PRICE_ID,
-          quantity: 1,
-        },
-      ],
+      line_items,
       mode: 'payment',
       success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin}/payment`
--- pages/payment.tsx
+++ pages/payment.tsx
@@ -9,6 +9,61 @@ import { MdOutlineArrowBack } from "react-icons/md";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+const otro = [
+  {
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        name: 'Papita.jpg',
+        images:['https://media.tenor.com/PZTvw4FUaTcAAAAM/le%C5%9F.gif'],
+      },
+      unit_amount: 2000,
+    },
+    quantity: 1,
+  },
+  {
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        name: 'Banana',
+        images:['https://media.tenor.com/4_E21LSI0ogAAAAj/banana-cheerer.gif'],
+      },
+      unit_amount: 500,
+    },
+    quantity: 2,
+  },
+  {
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        
+        name: 'Mouse',
+        images: ["https://http2.mlstatic.com/D_NQ_NP_2X_979137-MLA44255818618_122020-F.webp"],
+      },
+      unit_amount: 1500,
+    },
+    quantity: 1,
+  },
+]
+
+function conversion (cart:any) {
+  if(!cart.length) return
+  let array: any[] = []
+  for (let i = 0; i < cart.length; i++) {
+    array.push({
+      price_data:{
+        currency:'usd',
+        product_data: {
+          name: cart[i].title,
+          images:[cart[i].image]
+        },
+        unit_amount: cart[i].price * 100,
+      },
+      quantity: cart[i].qty
+    })
+  }
+  return array
+}
 
 export default function Payment() {
 
@@ -19,19 +74,19 @@ export default function Payment() {
     return total;
   }
   const handleClick = async (event: any) => {
+    const line_items = conversion(products)
     const { sessionId } = await fetch('http://localhost:3000/api/checkout_sessions', {
       method: 'POST',
       headers: {
         "content-type": "application/json"
       },
-      body: JSON.stringify({ quantity: 1 })
+      body: JSON.stringify(line_items)
     }).then(res => res.json());
     const stripe = await stripePromise;
     const { error }: any = await stripe?.redirectToCheckout({
       sessionId,
     });
   };
-
 
   return (
     <Layout>

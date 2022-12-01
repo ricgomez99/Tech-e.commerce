41aaf691437b6aa47ba4ec030e9b66599c2f07a5
Martina Girollet <martinagirolletgmail.com>
2022-12-01 11:32:10 -0300

    Payment Gateway almost done, some styles left

--- components/cardPayment.tsx
+++ components/cardPayment.tsx
@@ -9,7 +9,15 @@ export default function CardPayment() {
 
   const handleDelete = (product: any) => {
     products.deleteItem(product);
-  }
+  };
+
+  const handleAddProduct = (product : any) => {
+    products.addItemToCart(product);
+  };
+
+  const handleDeleteOneProduct = (product: any) => {
+    products.deletePerItem(product);
+  };
 
   return(
     <div>
@@ -21,13 +29,17 @@ export default function CardPayment() {
               alt={p.title}
               width={200}
               height={150}
+              className={styles.payImg}
             />
           </div>
-          <div className={styles.titleContainer}>
+          <div>
             <h5 className={styles.title}> {p.title}</h5>
-            {p.qty === 0 ? "" : <div>Units: {p.qty}</div>}
-            <h3 className={styles.price}>US${p.price}</h3>
+            <h4>US${p.price}</h4>
+            {p.qty === 0 ? "" : <div><h5>Units: {p.qty}</h5></div>}
+            {p.qty === 0 ? "" : <div><h4>Subtotal: US${p.qty * p.price}</h4></div>}
             <button className="btn btn-danger" onClick={() =>handleDelete(p.id)}><FaTrash/></button>
+            <button onClick={() => handleAddProduct(p)}>+</button>
+            <button onClick={() => handleDeleteOneProduct(p.id)}>-</button>
           </div>
         </div>
       ))}  

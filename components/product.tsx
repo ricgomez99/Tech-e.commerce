f2408f79aca7670352f083408bde9a0e1cdad384
Andres <ricardo.gomez.restrepo@gmail.com>
2022-11-30 20:17:59 -0500

    cart images scaled down

--- components/product.tsx
+++ components/product.tsx
@@ -8,7 +8,6 @@ import { useAppContext } from "../components/statewrapper";
 import { BsFillTrashFill } from "react-icons/bs";
 import { MdOutlineArrowBack } from "react-icons/md";
 
-
 type Data = {
   product: any;
   showAs: string;
@@ -22,9 +21,9 @@ export default function Product({ product, showAs, qty }: Data) {
     cart.addItemToCart(product);
   };
 
-  const handleDeletePerItem = (product : any) => {
+  const handleDeletePerItem = (product: any) => {
     cart.deletePerItem(product);
-  }
+  };
 
   const handleDelete = (product: any) => {
     cart.deleteItem(product);
@@ -35,7 +34,7 @@ export default function Product({ product, showAs, qty }: Data) {
       <>
         {/*  */}
         <button onClick={() => Router.back()} className="btn btn-secondary">
-              <MdOutlineArrowBack/>
+          <MdOutlineArrowBack />
         </button>
         {/*  */}
         <div className={style.page}>
@@ -75,24 +74,47 @@ export default function Product({ product, showAs, qty }: Data) {
             alt={product.title}
             width={100}
             height={100}
+            className={style.cartImg}
           />
         </div>
         <div className={style.content}>
           <h3>{product.title}</h3>
           <div className={style.detailContent}>
-          <div className="left">  
-          <div>${product.price}</div>
-          {qty === 0 ? "" : <div>Units: {qty}</div>}
-          {qty === 0 ? "" : <div>Subtotal: ${qty * product.price}</div>}
-          </div>
-          <div className="right">
-            {qty === 0 ? "" : 
-            <div className={style.buttons}>
-            <button onClick={() =>handleDelete(product.id)} className={style.button}><BsFillTrashFill /></button> 
-            <button onClick={() =>handleAddItem(product)} className={style.button}>+</button>
-            {qty === 1 ? "" : <button onClick={() =>handleDeletePerItem(product.id)} className={style.button}>-</button>}
-            </div>}
-          </div>
+            <div className="left">
+              <div>${product.price}</div>
+              {qty === 0 ? "" : <div>Units: {qty}</div>}
+              {qty === 0 ? "" : <div>Subtotal: ${qty * product.price}</div>}
+            </div>
+            <div className="right">
+              {qty === 0 ? (
+                ""
+              ) : (
+                <div className={style.buttons}>
+                  <button
+                    onClick={() => handleDelete(product.id)}
+                    className={style.button}
+                  >
+                    <BsFillTrashFill />
+                  </button>
+                  <button
+                    onClick={() => handleAddItem(product)}
+                    className={style.button}
+                  >
+                    +
+                  </button>
+                  {qty === 1 ? (
+                    ""
+                  ) : (
+                    <button
+                      onClick={() => handleDeletePerItem(product.id)}
+                      className={style.button}
+                    >
+                      -
+                    </button>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>

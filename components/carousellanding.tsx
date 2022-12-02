a7658f96f1520435639d6e598c6e0a43fbe13e2b
Andres <ricardo.gomez.restrepo@gmail.com>
2022-12-02 18:58:28 -0500

    categories section complete

--- components/carousellanding.tsx
+++ components/carousellanding.tsx
@@ -12,43 +12,74 @@ import st from "../styles/carouselLanding.module.css";
 
 export default function CarouselLanding() {
   return (
-    <div  className={st.carContainer}>
-      <h4>Categories</h4>
-    <Carousel fade className={st.car} variant="dark" interval = {null}>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={headphone} alt="Third slide" />
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={keyboard} alt="Third slide" />
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={chair} alt="Third slide" />
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={laptop} alt="Third slide" />
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={webcam} alt="Third slide" />
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={mouse} alt="First slide" />
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={display} alt="Second slide" />
+    <div className={st.carContainer}>
+      <Carousel carousel-fade className={st.car} variant="dark" interval={4000}>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={headphone}
+            alt="Third slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={keyboard}
+            alt="Third slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={chair}
+            alt="Third slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={laptop}
+            alt="Third slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={webcam}
+            alt="Third slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={mouse}
+            alt="First slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={display}
+            alt="Second slide"
+          />
 
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className={`d-block w.100 ${st.carouselImg}`} src={desktop} alt="Third slide" />
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className={`d-block w.100 ${st.carouselImg}`}
+            src={desktop}
+            alt="Third slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }

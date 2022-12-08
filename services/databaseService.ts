53da7d4f3c54dbf3749be02e7db20a6d1d19fade
Jotanjo <maza011000@gmail.com>
2022-12-08 18:37:55 -0300

    mody

--- services/databaseService.ts
+++ services/databaseService.ts
@@ -1,23 +1,17 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "lib/prisma";
 
-export default async function handlerGetUniqueUsers(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { id } = req.query;
-  const newId = Number(id);
-  try {
+export default async function handlerGetUniqueUsers() {
+
+   const user = async(id: string) => {
     const findUser = await prisma.user.findUnique({
       where: {
-        id: newId,
+        id: Number(id),
       },
       select: {
         email: true
       }
     });
-    return res.status(200).json(findUser);
-  } catch (error) {
-    return res.status(404).json({ message: "No users found" });
+    
   }
+    
 }

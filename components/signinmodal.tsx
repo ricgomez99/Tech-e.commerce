34d82d5992685fe205514be513b1706d735fe1c9
Jotanjo <maza011000@gmail.com>
2022-12-09 20:31:38 -0300

    adding some little things

--- components/signinmodal.tsx
+++ components/signinmodal.tsx
@@ -8,7 +8,9 @@ import userServiceFactory from "clientServices/userService";
 import useUser from "../lib/useUser";
 
 const userService = userServiceFactory();
-
+//localstorage {user : papita@gmail.com}
+//const user = localStorage.get("user")
+//{user ? lkasjdflaksdjfñlaskdf : null}
 
 
 export default function SignInModal() {

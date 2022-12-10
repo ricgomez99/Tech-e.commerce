1b4d5edc80d3a2e79910a972c69fffd7cce0a030
juanjobogado <juanjobogado13@gmail.com>
2022-12-10 12:22:49 -0300

    maybe now?

--- pages/api/updateUser.ts
+++ pages/api/updateUser.ts
@@ -14,7 +14,7 @@ export default async function handlerUpdateUser(
   // }
   try {
     const user = await prisma.user.update({
-     where: {id: id as string},
+     where: {id: id as any},
       data: {
         email: email,
         name: name,

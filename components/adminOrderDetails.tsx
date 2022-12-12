8a70013c9c9eb11c378b97c48ae8b79692bea0ee
Antonio Michel <antoniomichelgonzalez@gmail.com>
2022-12-12 15:08:56 -0600

    dashboard only missing order details

--- components/adminOrderDetails.tsx
+++ components/adminOrderDetails.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/ordersAdmin.module.css";
+import styles from "../styles/adminOrders.module.css";
 import { findSaleDetails, updateSale } from "services/saleEndPoints";
 import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
@@ -12,104 +12,102 @@ export default function AdminOrderDetails({ id }: any) {
   useEffect(() => {
     try {
       (async () => {
-        setOrder(await findSaleDetails(id));
+        id ? setOrder(await findSaleDetails(id)) : null;
       })();
     } catch (error) {
       console.log(error);
     }
   }, [id, state]);
 
-  async function handleClick(e: any) {
-    switch (e.target.value) {
-      case "success":
-        await statusAlert("success");
-        break;
-      case "pending":
-        await statusAlert("pending");
-        break;
-      case "failure":
-        await statusAlert("pending");
-        break;
-      default:
-        break;
-    }
-  }
+  // async function handleClick(e: any) {
+  //   switch (e.target.value) {
+  //     case "success":
+  //       await statusAlert("success");
+  //       break;
+  //     case "pending":
+  //       await statusAlert("pending");
+  //       break;
+  //     case "failure":
+  //       await statusAlert("pending");
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 
-  async function statusAlert(status: string) {
-    Swal.fire({
-      title:
-        status === "success"
-          ? "Set this sale's payment as SUCCESS?"
-          : status === "pending"
-          ? "Set this sale's payment as PENDING?"
-          : "Set this sale's payment as FAILURE?",
-      text: "",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes",
-      reverseButtons: true,
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        if (status === "success") {
-          Swal.fire("Sale payment was successful!", "", "success");
-          orderStatus(status);
-        } else if (status === "pending") {
-          Swal.fire("Sale payment is pending confirmation", "", "success");
-          orderStatus(status);
-        } else {
-          Swal.fire("Sale payment has failed", "", "success");
-          orderStatus(status);
-        }
-      }
-    });
-  }
+  // async function statusAlert(status: string) {
+  //   Swal.fire({
+  //     title:
+  //       status === "success"
+  //         ? "Set this sale's payment as SUCCESS?"
+  //         : status === "pending"
+  //         ? "Set this sale's payment as PENDING?"
+  //         : "Set this sale's payment as FAILURE?",
+  //     text: "",
+  //     icon: "warning",
+  //     showCancelButton: true,
+  //     confirmButtonColor: "#3085d6",
+  //     cancelButtonColor: "#d33",
+  //     confirmButtonText: "Yes",
+  //     reverseButtons: true,
+  //   }).then(async (result) => {
+  //     if (result.isConfirmed) {
+  //       if (status === "success") {
+  //         Swal.fire("Sale payment was successful!", "", "success");
+  //         orderStatus(status);
+  //       } else if (status === "pending") {
+  //         Swal.fire("Sale payment is pending confirmation", "", "success");
+  //         orderStatus(status);
+  //       } else {
+  //         Swal.fire("Sale payment has failed", "", "success");
+  //         orderStatus(status);
+  //       }
+  //     }
+  //   });
+  // }
 
-  async function orderStatus(status: string) {
-    switch (status) {
-      case "success":
-        await updateSale({ state: "SUCCESS" }, order.id);
-        setState(!state);
-        break;
+  // async function orderStatus(status: string) {
+  //   switch (status) {
+  //     case "success":
+  //       await updateSale({ state: "SUCCESS" }, order.id);
+  //       setState(!state);
+  //       break;
 
-      case "pending":
-        await updateSale({ state: "PENDING" }, order.id);
-        setState(!state);
-        break;
+  //     case "pending":
+  //       await updateSale({ state: "PENDING" }, order.id);
+  //       setState(!state);
+  //       break;
 
-      case "failure":
-        await updateSale({ state: "FAILURE" }, order.id);
-        setState(!state);
-        break;
+  //     case "failure":
+  //       await updateSale({ state: "FAILURE" }, order.id);
+  //       setState(!state);
+  //       break;
 
-      default:
-        break;
-    }
-  }
+  //     default:
+  //       break;
+  //   }
+  // }
 
   return (
-    <div className={styles.productsContainer}>
-      <div className={styles.productDetail}>
+    <div className={styles.ordersContainer}>
+      <div className={styles.orderDetail}>
         <h5>Order Detail</h5>
-        {order ? (
+        {id ? (
           <div className={styles.detail}>
-            <h3>Order ID: {order.id}</h3>
-            <h3>User: {order.userId}</h3>
-            <h3>Date: {order.date}</h3>
-            <h3>Sale total: $ {order.total}.00 USD</h3>
+            <h5>Order ID: {order.id}</h5>
+            <h5>User: {order.userId}</h5>
+            <h5>Date: {order.date}</h5>
+            <h5>Sale total: $ {order.total}.00 USD</h5>
 
             <div>
-              <ul>
-                {order.saleDetails?.map((p: any) => (
-                  <li key={p.id}>
-                    {p.id}, {p.amount}, {p.price}, {p.idProduct}
-                  </li>
-                ))}
-              </ul>
+              {order.saleDetails?.map((p: any) => (
+                <h6 key={p.id}>
+                  {p.id}, {p.amount}, {p.price}, {p.idProduct}
+                </h6>
+              ))}
             </div>
 
-            <h3>Status: {order.state}</h3>
+            {/* <h3>Status: {order.state}</h3>
             {order.state === "SUCCESS" ? null : (
               <button value="success" onClick={(e) => handleClick(e)}>
                 Mark as completed
@@ -124,7 +122,7 @@ export default function AdminOrderDetails({ id }: any) {
               <button value="failure" onClick={(e) => handleClick(e)}>
                 Mark as failed
               </button>
-            )}
+            )} */}
           </div>
         ) : (
           <div className={styles.detail}>
--- components/adminProducts.tsx
+++ components/adminProducts.tsx
@@ -6,7 +6,6 @@ import SearchBar from "./searchbar";
 import Product from "./product";
 
 export default function AdminProducts() {
-
   const [products, setProducts] = useState<any[]>([]);
   const [conditions, setConditions] = useState({});
   const [chosenProduct, setChosenProduct] = useState({});
@@ -30,45 +29,59 @@ export default function AdminProducts() {
     }
   }, [conditions]);
 
-  return(
+  return (
     <div className={styles.title}>
       <h3>Products</h3>
       <div className={styles.productsContainer}>
         <div className={styles.productDetail}>
           <h5>Product Detail</h5>
           <div className={styles.detail}>
-            {Object.keys(chosenProduct).length ?(<Product 
-              product={chosenProduct} 
-              qty={undefined}
-              showAs="adminProduct"
-              />) : ""}
+            {Object.keys(chosenProduct).length ? (
+              <Product
+                product={chosenProduct}
+                qty={undefined}
+                showAs="adminProduct"
+              />
+            ) : (
+              <h4>Choose a product from the list</h4>
+            )}
           </div>
         </div>
         <div className={styles.allProducts}>
           <div className={styles.search}>
             <SearchBar handleConditions={handleConditions} />
-            <button className={styles.refresh} onClick={() => setConditions({})}>
-            Refresh
+            <button
+              className={styles.refresh}
+              onClick={() => setConditions({})}
+            >
+              Refresh
             </button>
           </div>
           <div className={styles.all}>
-              {products?.map((p) =>(
-                <div key={p.id} className={styles.products}>
-                  <div>
-                    <Image
-                      width={100}
-                      height={100}
-                      src={p.image}
-                      alt={p.title}
-                      className={styles.productImg}
-                    />
-                  </div>
-                  <h5 className={styles.name} onClick={() => {handleProduct(p)}}>{p.title}</h5>
-                </div>  
-              ))}
+            {products?.map((p) => (
+              <div key={p.id} className={styles.products}>
+                <div>
+                  <Image
+                    width={100}
+                    height={100}
+                    src={p.image}
+                    alt={p.title}
+                    className={styles.productImg}
+                  />
+                </div>
+                <h5
+                  className={styles.name}
+                  onClick={() => {
+                    handleProduct(p);
+                  }}
+                >
+                  {p.title}
+                </h5>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
-};
+}

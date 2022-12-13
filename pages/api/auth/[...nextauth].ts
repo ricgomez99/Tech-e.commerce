065979b192b0cf2b320429fc8ea3b766aa7f4528
Jotanjo <maza011000@gmail.com>
2022-12-13 11:00:06 -0300

    nothing

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "lib/prisma";
-
+  
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [

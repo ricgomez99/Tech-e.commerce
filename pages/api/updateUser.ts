58c8a35b667580d763d88a4c30650c746df1bf07
Jotanjo <maza011000@gmail.com>
2022-12-14 13:35:35 -0300

    decilo

--- pages/api/updateUser.ts
+++ pages/api/updateUser.ts
@@ -7,20 +7,19 @@ export default async function handlerUpdateUser(
 ) {
   const { role, active } = req.body;
   const { id } = req.query;
-  
+
   try {
     const user = await prisma.user.update({
-     where: {id: id?.toString()},
+      where: { id: id?.toString() },
       data: {
         role: role,
-        active: active
+        active: active,
       },
     });
     return res.status(200).send(user);
-  }  catch (error) {
-    if(error instanceof Error){
-    return res.status(400).json({ message: error.message });
-    }
-    else return res.status(404).json({message: "error not found"})
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ message: error.message });
+    } else return res.status(404).json({ message: "error not found" });
   }
 }

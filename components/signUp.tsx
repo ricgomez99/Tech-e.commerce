cd7023901f5f3c0951cb2652b9fd020a2e41d3e7
Andres <ricardo.gomez.restrepo@gmail.com>
2022-12-14 20:29:44 -0500

    signUp form stykes applied

--- components/signUp.tsx
+++ components/signUp.tsx
@@ -3,6 +3,8 @@ import { Formik, Field, ErrorMessage, Form } from "formik";
 import { createUser } from "../services/userEndPoints";
 import * as yup from "yup";
 import { useRouter } from "next/router";
+import styles from "../styles/signUp.module.css";
+import Image from "next/image";
 
 export default function SignIn() {
   const [submitted, setSubmitted] = useState(false);
@@ -19,50 +21,69 @@ export default function SignIn() {
   };
   const router = useRouter();
   return (
-    <div>
-      <Formik
-        initialValues={{
-          email: "",
-          username: "",
-          password: "",
-        }}
-        validationSchema={yup.object({
-          email: yup.string().required("Email is required."),
-          username: yup.string().required("User is required"),
-          password: yup.string().required("Password is required"), //Will we be able to add
-        })}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <div className="mb-3">
-            <Field type="text" name="email" placeholder="user@email.com" />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className="text-danger"
-            />
-          </div>
-          <div className="mb-3">
-            <Field type="text" name="username" placeholder="Username..." />
-            <ErrorMessage
-              name="username"
-              component="div"
-              className="text-danger"
-            />
-          </div>
-          <div className="mb-3">
-            <Field type="password" name="password" placeholder="Password..." />
-            <ErrorMessage
-              name="password"
-              component="div"
-              className="text-danger"
-            />
-          </div>
-          <button type="submit" value="Login">
-            Register!
-          </button>
-        </Form>
-      </Formik>
+    <div className={styles.card}>
+      <Image
+        src="/Img/signUp.svg"
+        alt="sign up"
+        width={350}
+        height={100}
+        className={styles.image}
+      />
+      <div className={styles.content}>
+        <Formik
+          initialValues={{
+            email: "",
+            username: "",
+            password: "",
+          }}
+          validationSchema={yup.object({
+            email: yup.string().required("Email is required."),
+            username: yup.string().required("User is required"),
+            password: yup.string().required("Password is required"), //Will we be able to add
+          })}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <div className={styles.body}>
+              <div className={`mb-2 ${styles.input}`}>
+                <Field type="text" name="email" placeholder="user@email.com" />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className={styles.errorTxt}
+                />
+              </div>
+              <div className={`mb-2 ${styles.input}`}>
+                <Field type="text" name="username" placeholder="Username..." />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className={styles.errorTxt}
+                />
+              </div>
+              <div className={`mb-2 ${styles.input}`}>
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Password..."
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className={styles.errorTxt}
+                />
+              </div>
+              <button
+                className={styles.registerBtn}
+                type="submit"
+                value="Login"
+              >
+                <span>Register</span>
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 }
